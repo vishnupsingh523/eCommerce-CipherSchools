986b1d4765e067a33e6a1130eaa3a92f12d62a79
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-07-16 23:49:34 +0530

    files added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import {sliderItems} from "../data/sliderItems";
 
 const Container = styled.div`
 	width: 100%;
@@ -8,6 +9,7 @@ const Container = styled.div`
 	display: flex;
 	background-color: coral;
 	position: relative;
+	overflow-x: hidden;
 `;
 
 const Arrow = styled.div`
@@ -18,7 +20,7 @@ const Arrow = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	position: absolute;
+	position: absolute;	
 	top: 0;
 	bottom: 0;
 	left: ${(props) => props.direction === "left" && "10px"};
@@ -26,44 +28,93 @@ const Arrow = styled.div`
 	margin: auto;
 	cursor: pointer;
 	opacity: 0.5;
+	z-index:2;
 `;
 
 const Wrapper = styled.div`
 	height: 100%;
+	display:flex;
+	transform:translateX(${props=>props.slideIndex * -100}vw);
 `;
 const Slide = styled.div`
 	width: 100vw;
 	height: 100vh;
 	display: flex;
 	align-items: center;
+	background-color: #{(props)=>props.bg};
+`;
+const ImageContainer = styled.div`
+	height: 100%;
+	flex: 1;
 `;
 const Image = styled.img`
 	height: 80%;
-`;
-const ImageContainer = styled.div`
-	flex: 1;
-`;
-const InfoContainer = styled.div`
-	flex: 1;
+	/* align-self: center; */
 `;
 
+const InfoContainer = styled.div`
+	flex: 1;
+	padding: 50px;
+`;
+
+const Title = styled.h1`
+	font-size:70px;
+`
+const Desc = styled.p`
+	margin: 50px 0px;
+	font-size: 20px;
+	font-weight: 500;
+	letter-spacing: 3px;
+`
+const Button = styled.button`
+	padding: 10px;
+	font-size: 20px;
+	background-color: transparent;
+	cursor:pointer;
+`
+
+
 const Slider = () => {
+	const [slideIndex, setSlideIndex] = useState(0);
+	
+	const handleClick = (direction) => {
+		if (direction === "left") {
+			setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+		}
+		else {
+			setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+		}
+	};
+
 	return (
 		<Container>
-			<Arrow direction="left">
+			<Arrow direction="left" onClick={()=>handleClick("left")}>
 				<ArrowLeftOutlined />
 			</Arrow>
 
-			<Wrapper>
-				<Slide>
-					<ImageContainer>
-						<Image src="https://i.pinimg.com/originals/fb/64/03/fb6403e657ab53c877678c297e3b42e0.jpg" />
-					</ImageContainer>
-					<InfoContainer></InfoContainer>
-				</Slide>
+			<Wrapper slideIndex = {slideIndex}>
+				{
+					sliderItems.map((item) => {
+						return (
+							<Slide bg={item.bg}>
+								<ImageContainer>
+									<Image src={item.img} />
+								</ImageContainer>
+								<InfoContainer>
+										<Title>
+											{item.title}
+									</Title>
+									<Desc>{item.desc}</Desc>
+									<Button>SHOP NOW</Button>
+								</InfoContainer>
+							</Slide>
+					)
+					})
+				}
+				
 			</Wrapper>
 
-			<Arrow direction="right">
+			<Arrow direction="right" onClick={()=>handleClick("right")}>
 				<ArrowRightOutlined />
 			</Arrow>
 		</Container>
---
+++ src/data/sliderItems.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+	{
+		id: 1,
+		img: "https://i.pinimg.com/originals/fb/64/03/fb6403e657ab53c877678c297e3b42e0.jpg",
+		title: "SUMMER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		img: "https://img.freepik.com/free-photo/two-beautiful-women-shopping-town_1303-16426.jpg?w=2000",
+		title: "BIG BILLION DAY SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		img: "https://images.unsplash.com/photo-1548142693-a5ef9e3d0e5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHMlMjBzaG9wcGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+		title: "WINTER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f5fafd",
+	},
+]
