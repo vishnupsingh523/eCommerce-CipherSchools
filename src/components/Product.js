1f57940c9ab876dfbc872519accee65007569f4e
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-07-23 09:43:36 +0530

    files added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,46 +1,97 @@
-import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons"
+import {
+	FavoriteBorderOutlined,
+	SearchOutlined,
+	ShoppingCartOutlined,
+} from "@material-ui/icons";
 import styled from "styled-components";
 
 
+const Info = styled.div`
+	opacity: 0;
+	width:100%;
+	height:100%;
+	position:absolute;
+	top: 0;
+	left: 0;
+	background-color: rgba(0,0,0, 0.2);
+	z-index : 3;
+	display:flex;
+	align-items: center;
+	justify-content: center;
+	transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
-	flex:1;
-	margin:5px;
+	flex: 1;
+	margin: 5px;
 	min-width: 280px;
 	height: 350px;
-	display:flex;
-	align-items:center;
-	justify-content:center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
 	background-color: #f5fbfd;
-`
+	position:relative;
+
+	&:hover ${Info}{
+	opacity: 1;
+	width:100%;
+	height:100%;
+	position:absolute;
+	top: 0;
+	left: 0;
+	background-color: rgba(0,0,0, 0.2);
+	z-index : 3;
+	display:flex;
+	align-items: center;
+	justify-content: center;
+	}
+`;
+const ImageBox = styled.div`
+	/* width: 100%; */
+	overflow: hidden;
+	/* text-align:center; */
+	/* align-items:center; */
+`;
 const Image = styled.img`
-	height:75%;
-`
-const Info = styled.div`
-
-`
+	width: 350px;
+`;
 const Icon = styled.div`
+	width:40px;
+	height:40px;
+	border-radius: 50%;
+	background-color: white;
+	display: flex;
+	align-items:center;
+	justify-content: center;
+	margin: 10px;
+	transition: all 0.5s ease ;
+	&:hover{
+		background-color: #e9f5f5;
+		transform: scale(1.1);
+		cursor:pointer;
+	}
+`;
 
+const Product = ({ item }) => {
+	return (
+		<Container>
+			<ImageBox>
+				<Image src={item.img} />
+			</ImageBox>
+			<Info>
+				<Icon>
+					<ShoppingCartOutlined />
+				</Icon>
+				<Icon>
+					<SearchOutlined />
+				</Icon>
+				<Icon>
+					<FavoriteBorderOutlined />
+				</Icon>
+			</Info>
+		</Container>
+	);
+};
 
-`
-
-const Product = ({item}) => {
-  return (
-	<Container>
-		  <Image src={item.img} />
-		  <Info>
-			  <Icon>
-				  <ShoppingCartOutlined />
-			  </Icon>
-			  <Icon>
-				  <SearchOutlined />
-			  </Icon>
-			  <Icon>
-				  <FavoriteBorderOutlined />
-			  </Icon>
-		  </Info>
-
-	</Container>
-  )
-}
-
-export default Product
+export default Product;
